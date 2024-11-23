3646cf57b74796b46d2fbd67312fd901183236c4
Nidhi M D <166122007+nidhi-mundaragi@users.noreply.github.com>
2024-11-23 12:19:22 +0530

    Update fp_int2float.v

--- fp_int2float.v
+++ fp_int2float.v
@@ -6,16 +6,16 @@ module fp_int2float(
     reg [8:0] mantissa;    
     reg sign;             
     reg [31:0] abs_input;
+    reg done;
+    integer i;
     
     always @(*) begin
        
-      if (in_int == 16'b0) begin
+    
+      if (in_int == 32'b0) begin
         float_out = 16'b0;
       end
-      else if (in_int == 16'hFFFF) begin
-        float_out = 16'hFFFF;
-      end
-      else begin
+      
       sign = (in_int < 0) ? 1 : 0;
         
       //determine absolute value
@@ -24,19 +24,31 @@ module fp_int2float(
         // Normalize the number 
         exponent = 0;
         mantissa = 0;
-        
-      // Find the exponent (shift the number to be in the form 1.xxxx)
-        while (abs_input >= (1 << (exponent + 1))) begin
+        done = 0;
+      
+      
+     // NOTE: while loop is not synthesisable so changed it to for loop
+      //Note: might give warning for else block missing but tried exiting the loop with flag in the else block gave synth errors
+       for (i = 0; i < 32 && !done; i = i + 1) begin
+        if (abs_input >= (1 << (exponent + 1))) begin
             exponent = exponent + 1;
         end
+    end
+      
+      
+      
         
         // Shift the number to form the normalized mantissa
-        mantissa = abs_input << (9 - exponent);
+        if ( exponent <= 9) begin
+          mantissa = abs_input << (9 - exponent);  // Left shift for +ve exp
+           end else begin
+             mantissa = abs_input >> (exponent - 9);// Right shift for -ve exp
+           end
+
         
         //Bias the exponent 
         exponent = exponent + 31;
-        
+      
       float_out = {sign,exponent,mantissa};
-      end
     end
 endmodule
